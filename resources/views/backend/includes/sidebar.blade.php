df972f73fb22541262809cce6fc0401f3bf4d2e6
mcankaya4 <mcankaya4@gmail.com>
2022-07-18 22:17:09 +0300

    side bar - header düzenlendi

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -4,43 +4,84 @@
     <div class="menu">
         <ul class="list">
             <li class="header">Menü</li>
-            <li class="active">
-                <a href="#" onClick="return false;" class="menu-toggle">
+            <li class="{{ Route::currentRouteNamed('admin.index') ? 'active' : '' }}">
+                <a href="{{ route('admin.index') }}">
                     <i data-feather="home"></i>
                     <span>Anasayfa</span>
                 </a>
+            </li>
+            <li class="{{ Request::is(['admin/about/*','admin/about']) ? 'active' : '' }}">
+                <a href="#" onClick="return false;" class="menu-toggle">
+                    <i data-feather="archive"></i>
+                    <span>Hakkımda Bölümü</span>
+                </a>
                 <ul class="ml-menu">
-                    <li>
-                        <a href="../../index.html">Dashboard 1</a>
-                    </li>
-                    <li>
-                        <a href="../../pages/dashboard/dashboard2.html">Dashboard 2</a>
+                    <li class="{{ Route::currentRouteNamed('admin.about.edit') ? 'active' : '' }}">
+                        <a href="{{ route('admin.about.edit') }}">Düzenle</a>
                     </li>
                 </ul>
             </li>
-            <li>
-                <a href="../../pages/apps/calendar.html">
-                    <i data-feather="calendar"></i>
-                    <span>Events</span>
-                </a>
-            </li>
-            <li>
+            <li class="{{ Request::is(['admin/slider/*','admin/slider']) ? 'active' : '' }}">
                 <a href="#" onClick="return false;" class="menu-toggle">
-                    <i data-feather="box"></i>
-                    <span>Extra Pages</span>
+                    <i data-feather="sliders"></i>
+                    <span>Slider Bölümü</span>
                 </a>
                 <ul class="ml-menu">
-                    <li>
-                        <a href="../../pages/examples/profile.html">Profile</a>
+                    <li class="{{ Route::currentRouteNamed('admin.slider.edit') ? 'active' : '' }}">
+                        <a href="{{ route('admin.slider.edit') }}">Düzenle</a>
                     </li>
-                    <li>
-                        <a href="../../pages/examples/pricing.html">Pricing</a>
+                </ul>
+            </li>
+            <li class="{{ Request::is(['admin/breadcrumb/*','admin/breadcrumb']) ? 'active' : '' }}">
+                <a href="#" onClick="return false;" class="menu-toggle">
+                    <i data-feather="image"></i>
+                    <span>İconlar Bölümü</span>
+                </a>
+                <ul class="ml-menu">
+                    <li class="{{ Route::currentRouteNamed('admin.breadcrumb.index') ? 'active' : '' }}">
+                        <a href="{{ route('admin.breadcrumb.index') }}">Listele</a>
                     </li>
-                    <li>
-                        <a href="../../pages/examples/faqs.html">Faqs</a>
+                    <li class="{{ Route::currentRouteNamed('admin.breadcrumb.create') ? 'active' : '' }}">
+                        <a href="{{ route('admin.breadcrumb.create') }}">Ekle</a>
                     </li>
-                    <li class="active">
-                        <a href="../../pages/examples/blank.html">Blank Page</a>
+                </ul>
+            </li>
+            <li class="{{ Request::is(['admin/portfolio/*','admin/portfolio']) ? 'active' : '' }}">
+                <a href="#" onClick="return false;" class="menu-toggle">
+                    <i data-feather="bookmark"></i>
+                    <span>Portfolyolar Bölümü</span>
+                </a>
+                <ul class="ml-menu">
+                    <li class="{{ Route::currentRouteNamed('admin.portfolio.index') ? 'active' : '' }}">
+                        <a href="{{ route('admin.portfolio.index') }}">Listele</a>
+                    </li>
+                    <li class="{{ Route::currentRouteNamed('admin.portfolio.create') ? 'active' : '' }}">
+                        <a href="{{ route('admin.portfolio.create') }}">Ekle</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="{{ Request::is(['admin/category/*','admin/category']) ? 'active' : '' }}">
+                <a href="#" onClick="return false;" class="menu-toggle">
+                    <i data-feather="list"></i>
+                    <span>Kategoriler Bölümü</span>
+                </a>
+                <ul class="ml-menu">
+                    <li class="{{ Route::currentRouteNamed('admin.category.index') ? 'active' : '' }}">
+                        <a href="{{ route('admin.category.index') }}">Listele ~ Ekle</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="{{ Request::is(['admin/service/*','admin/service']) ? 'active' : '' }}">
+                <a href="#" onClick="return false;" class="menu-toggle">
+                    <i data-feather="briefcase"></i>
+                    <span>Hizmetler Bölümü</span>
+                </a>
+                <ul class="ml-menu">
+                    <li class="{{ Route::currentRouteNamed('admin.service.index') ? 'active' : '' }}">
+                        <a href="{{ route('admin.service.index') }}">Listele</a>
+                    </li>
+                    <li class="{{ Route::currentRouteNamed('admin.service.create') ? 'active' : '' }}">
+                        <a href="{{ route('admin.service.create') }}">Ekle</a>
                     </li>
                 </ul>
             </li>
